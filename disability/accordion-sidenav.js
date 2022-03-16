057f491c0c41e9c77c5946e993be5b184bbfff86
Gagandeep Singh <gsingh@calarts.edu>
2022-03-16 12:47:06 -0700

    update with new code for dso' gpsh

---
+++ disability/accordion-sidenav.js
@@ -0,0 +1,71 @@
+$("ul.vertical-nav > li:nth-of-type(4) > a").click(function () {
+  var current = 'collapse8502';
+  var acc = document.querySelectorAll(`[data-target='#${current}']`);
+  var post = document.querySelectorAll(`#${current}`);
+  $(post).addClass('in');
+  $(acc).removeClass("collapsed");
+  $("html, body").animate({
+    scrollTop: $(acc).offset().top - 175
+  }, 1500);
+});
+
+$("ul.vertical-nav > li:nth-of-type(5) > a").click(function () {
+  var current = 'collapse8497';
+  var acc = document.querySelectorAll(`[data-target='#${current}']`);
+  var post = document.querySelectorAll(`#${current}`);
+  $(post).addClass('in');
+  $(acc).removeClass("collapsed");
+  $("html, body").animate({
+    scrollTop: $(acc).offset().top - 175
+  }, 1500);
+});
+$("ul.vertical-nav > li:nth-of-type(6) > a").click(function () {
+  var current = 'collapse8498';
+  var acc = document.querySelectorAll(`[data-target='#${current}']`);
+  var post = document.querySelectorAll(`#${current}`);
+  $(post).addClass('in');
+  $(acc).removeClass("collapsed");
+  $("html, body").animate({
+    scrollTop: $(acc).offset().top - 175
+  }, 1500);
+});
+$("ul.vertical-nav > li:nth-of-type(7) > a").click(function () {
+  var current = 'collapse8499';
+  var acc = document.querySelectorAll(`[data-target='#${current}']`);
+  var post = document.querySelectorAll(`#${current}`);
+  $(post).addClass('in');
+  $(acc).removeClass("collapsed");
+  $("html, body").animate({
+    scrollTop: $(acc).offset().top - 175
+  }, 1500);
+});
+$("ul.vertical-nav > li:nth-of-type(8) > a").click(function () {
+  var current = 'collapse8500';
+  var acc = document.querySelectorAll(`[data-target='#${current}']`);
+  var post = document.querySelectorAll(`#${current}`);
+  $(post).addClass('in');
+  $(acc).removeClass("collapsed");
+  $("html, body").animate({
+    scrollTop: $(acc).offset().top - 175
+  }, 1500);
+});
+$("ul.vertical-nav > li:nth-of-type(9) > a").click(function () {
+  var current = 'collapse8501';
+  var acc = document.querySelectorAll(`[data-target='#${current}']`);
+  var post = document.querySelectorAll(`#${current}`);
+  $(post).addClass('in');
+  $(acc).removeClass("collapsed");
+  $("html, body").animate({
+    scrollTop: $(acc).offset().top - 175
+  }, 1500);
+});
+$("ul.vertical-nav > li:nth-of-type(10) > a").click(function () {
+  var current = 'collapse8509';
+  var acc = document.querySelectorAll(`[data-target='#${current}']`);
+  var post = document.querySelectorAll(`#${current}`);
+  $(post).addClass('in');
+  $(acc).removeClass("collapsed");
+  $("html, body").animate({
+    scrollTop: $(acc).offset().top - 175
+  }, 1500);
+});
